c7745ce64bb1cf9e678ca43c99c9ab146067ce2d
Nicolas Pereyra <nicolasmpereyra96@gmail.com>
2022-06-07 21:28:39 -0300

    Primer ejercicio JS

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Convertidor de Pesos a Dolares
+
+let dolar = parseFloat(prompt("Ingrese el valor del dolar: "));
+let pesos = parseFloat(prompt("Cuantos pesos quiere convertir a dolar?"));
+let calculo = pesos / dolar;
+alert("Sus pesos equivalen a :" + calculo + " dolares.");
